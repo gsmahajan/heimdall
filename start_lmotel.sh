4f1813143bad9ff94424496ea1a3dc575642c427
Girish Mahajan <girish.mahajan@logicmonitor.com>
2022-06-27 16:58:29 +0530

    Foo

--- start_lmotel.sh
+++ start_lmotel.sh
@@ -4,6 +4,9 @@ cd /home/ubuntu/logicmonitor/logicmonitor/lmotel/
 
 export LOGICMONITOR_ACCOUNT="qauattraces01"
 
-./lmotel --config config.yaml
+Date=$(date +%d%m%Y)
+#rm -rf *.log
+
+./lmotel --config config.yaml >> logs_$Date.log 2>&1 &
 
 
