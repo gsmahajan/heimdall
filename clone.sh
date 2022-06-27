c0e8d9fc06a860d8856411d9ee21c9b53295f8ba
Girish Mahajan <girish.mahajan@logicmonitor.com>
2022-06-27 16:46:33 +0530

    foo

--- clone.sh
+++ clone.sh
@@ -4,7 +4,7 @@
 
 cd /home/ubuntu/logicmonitor/
 
-git clone https://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@github.com/gsmahajan/heimdall.git
+git clone https://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@github.com/gsmahajan/heimdall.git
 
 cd heimdall/apps/java/
 
